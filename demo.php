50cbb5e9e0e9cb1a40946d4cbd0a5f50f31e2936
tserlis <erlist@tcd.ie>
2018-02-08 19:20:00 +0000

    Add files via upload

---
+++ demo.php
@@ -0,0 +1,30 @@
+<?php
+
+define('DB_NAME', 'ferulatest');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+$link = mysqli_connect (DB_HOST, DB_USER, DB_PASSWORD);
+
+if (!$link) {
+  die('Could not connect: ' . mysqli_error());
+}
+
+$db_selected = mysqli_select_db($link, DB_NAME);
+
+if (!$db_selected) {
+  die('Can\'t use ' . DB_NAME . ' : ' . mysqli_error());
+}
+
+$value = $_POST["input1"];
+
+$sql = "INSERT INTO demo (input1) VALUES ('$value')";
+
+if (!mysqli_query($link, $sql, MYSQLI_STORE_RESULT)) {
+  die ('Error: ' . mysqli_error());
+}
+
+Echo "It's all legit mate ";
+mysqli_close($link);
+ ?>
